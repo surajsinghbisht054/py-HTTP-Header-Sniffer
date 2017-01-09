0da440646823704b38826c5808c3d0c0e0d8cb24
Suraj Singh Bisht <surajsinghbisht054@gmail.com>
2017-01-09 19:56:38 +0530

    Latest Version

--- httpHeaderCapture/pyhttpheader.py
+++ httpHeaderCapture/pyhttpheader.py
@@ -1,5 +1,4 @@
-# py-HTTP-Header-Sniffer
-
+#!/usr/bin/python
 # ---------------- READ ME ---------------------------------------------
 # This Script is Created Only For Practise And Educational Purpose Only
 # This Script Is Created For https://bitforestinfo.blogspot.in
@@ -20,11 +19,24 @@ __author__='''
 ######################################################
 '''
 
-# Usage:
-	python pyhttpheader.py (Use Administrator Privilage)
+import socket
 
-Please Feel Free For Contribution.
+s=socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
 
-Written By 
-	S.S.B
-	surajsinghbisht054@gmail.com
+while True:
+	data=s.recvfrom(65565)
+	try:
+		if "HTTP" in data[0][54:]:
+				print "[","="*30,']'
+				raw=data[0][54:]
+				if "\r\n\r\n" in raw:
+					line=raw.split('\r\n\r\n')[0]
+					print "[*] Header Captured "
+					print line[line.find('HTTP'):]
+				else:
+					print raw
+		else:
+			#print '[{}]'.format(data)
+			pass
+	except:
+		pass
